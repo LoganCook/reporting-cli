3b0e0dd1d42c4c21013a0e78b132278eb40114e0
Logan Cook <logan.cook@ersa.edu.au>
2018-03-16 10:10:04 +1030

    delete encoding lines as will not connect to HCP

--- ersa_reporting_kafka/archive/__init__.py
+++ ersa_reporting_kafka/archive/__init__.py
@@ -56,8 +56,6 @@ class Archive:
     re_offset = re.compile(r".*/[0-9]+-([0-9]+)\..+")
 
     def __init__(self, aws_id, aws_secret, server, bucket, object_prefix=""):
-        aws_id = base64.b64encode(aws_id)
-        aws_secret = hashlib.md5(aws_secret).hexdigest()
         hs3 = S3Connection(aws_access_key_id=aws_id,
                            aws_secret_access_key=aws_secret,
                            host=server)
